7d960a48b75bc4aa37cd080442950f5089ab3b9d
Mr.Holakk <45064556+MrHolako@users.noreply.github.com>
2018-11-15 23:49:50 +0200

    add

---
+++ Install Metasploit.sh
@@ -0,0 +1,92 @@
+#!bin/bash
+
+red='\e[1;31m'
+green='\e[1;32m'
+blue='\e[1;34m'
+purple='\e[1;35m'
+cyan='\e[1;36m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+rm -rif 4.17.14.tar.gz
+rm -rif metasploit.sh
+rm -rif metasploit-framework
+clear
+echo 
+echo
+echo 
+echo -e $red "™™™™™™™™™™™™™™™™™™™™™™™™™™™™™™™™™™™™™™™™™™™™™™™™"
+echo -e $green
+figlet "Start Install"
+echo 
+echo -e $red "™™™™™™™™™™™™™™™™™™™™™™™™™™™™™™™™™™™™™™™™™™™™™™™™"
+echo -e $white
+apt update && apt upgrade -y
+y
+apt install git
+y
+apt install python3
+y
+apt install python
+y
+apt install ruby
+y
+apt install wget
+y
+apt install unzip
+y
+apt install zip
+y
+apt install nano
+y
+apt install perl
+y
+apt install curl
+y
+apt install proot
+y
+cd $HOME
+wget https://Auxilus.github.io/metasploit.sh
+bash metasploit.sh
+cd $HOME
+cd metasploit-framework
+gem install bundle
+pkg install bundle
+y
+apt install bundle
+y
+gem install bundler
+pip2 install bundler
+pip2 install bundle
+gem install bundle
+bundle config build.nokogiri --use-system-libraries
+bundle install
+bundle update nokogiri
+gem install nokogiri
+gem install nokogiri -- --use-system-libraries
+gem install pkg-config -v ~> 1.1
+pkg-config
+bundle update nokogiri
+pg_ctl -D $PREFIX/var/lib/postgresql start
+clear
+clear
+echo
+echo
+echo
+echo -e $yellow
+figlet "Completed"
+echo
+echo -e $red "#############################"
+echo -e $green "# Author   : Mr.Holako      #"
+echo: e $red "#                           #"
+echo -e $green "# from     : Syria          #"
+echo: e $red "#                           #"
+echo -e $green "# WhatsApp : 00963954698951 #"
+echo: e $red "#                           #"
+echo -e $green "# Twitter  : Mr.Holako      #"
+echo -e $red "#############################"
+echo -e $white
+echo
+cd $HOME
+rm -rif Install-Metasploit-Auto
+cd metasploit-framework
+./msfconsole
